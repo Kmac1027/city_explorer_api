a22758ff98bd53580b1fd3402717e6c35ca750f3
Kevin McNeil <dogwoodrawks@gmail.com>
2020-09-14 12:47:22 -0700

    made server

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 'use strict'
 
 require('dotenv').config();
+const cors = require('cors');
 const PORT = process.env.PORT;
 const express = require('express');
 const app = express();
 
-app.use(expres.static ('./public'));
+//Routes
+app.use('*', notFound);
 
+app.get('/', (request, response) => {
+  console.log("alive?")
+})
+
+
+//functions
+function notFound {
+  response.status(404).send('Sorry, Not Found');
+}
+
+
+
+//server is listening
 app.listen(PORT, () => {
   console.log(`Server is ALIVE and listening on port ${PORT}`)
 })
