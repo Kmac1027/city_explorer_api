8efcdfbca710026e35ffd5758156d7910b8dcbb9
Kevin McNeil <dogwoodrawks@gmail.com>
2020-09-14 14:49:10 -0700

    fixed pathing

--- server.js
+++ server.js
@@ -5,24 +5,40 @@ const cors = require('cors');
 const PORT = process.env.PORT;
 const express = require('express');
 const app = express();
-
-//Routes
-app.use('*', notFound);
 app.use(cors());
 
-app.get('/', (request, response) => {
-  console.log("alive?");
-  // response.sendFile('./public/index.html');
-  response.send('hello world');
-})
+//Routes
 
+app.get('/location', locationHandler);
+app.use('*', notFound);
+
+// constructor function
+function Location(city, data) {
+  this.search_query = city;
+  this.formatted_query = data.display_name;
+  this.latitude = data.lat;
+  this.longitude = data.lon;
+}
 
 //functions
-function notFound() {
+function notFound(request, response) {
   response.status(404).send('Sorry, Not Found');
 }
 
-
+function locationHandler(request, response) {
+  try {
+    const city = request.query.city;
+    const data = require('./data/location.json');
+    console.log(data);
+    const locationData = new Location(city, data[0]);
+    console.log(locationData);
+    response.status(200).json(locationData);
+  }
+  catch (error) {
+    console.log('ERROR', error);
+    response.status(500).send('So sorry, something went wrong.');
+  }
+}
 
 //server is listening
 app.listen(PORT, () => {
